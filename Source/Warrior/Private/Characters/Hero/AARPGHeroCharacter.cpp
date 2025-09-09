3b877a6679aef5bdaba3d7479fa7cba7561e394e
DreadDev1 <kameen.avery@gmail.com>
2025-09-08 20:31:29 -0400

    Section 1 Lesson 9 Debug Helper

--- Source/Warrior/Private/Characters/Hero/AARPGHeroCharacter.cpp
+++ Source/Warrior/Private/Characters/Hero/AARPGHeroCharacter.cpp
@@ -3,30 +3,12 @@
 
 #include "Characters/Hero/AARPGHeroCharacter.h"
 
+#include "AARPGDebugHelper.h"
 
-// Sets default values
-AAARPGHeroCharacter::AAARPGHeroCharacter()
-{
-	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-}
-
-// Called when the game starts or when spawned
 void AAARPGHeroCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+
+	Debug::Print(TEXT("Working"));
 	
 }
-
-// Called every frame
-void AAARPGHeroCharacter::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-}
-
-// Called to bind functionality to input
-void AAARPGHeroCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-}
-
