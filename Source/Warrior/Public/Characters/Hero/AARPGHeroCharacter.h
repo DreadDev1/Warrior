c069d4d8a105989b8ab30dcde16f7535a8bd1156
DreadDev1 <kameen.avery@gmail.com>
2025-09-08 20:48:22 -0400

    Section 1 Lesson 10 Setup Hero Character

--- Source/Warrior/Public/Characters/Hero/AARPGHeroCharacter.h
+++ Source/Warrior/Public/Characters/Hero/AARPGHeroCharacter.h
@@ -6,11 +6,28 @@
 #include "Characters/AARPGBaseCharacter.h"
 #include "AARPGHeroCharacter.generated.h"
 
+class USpringArmComponent;
+class UCameraComponent;
+
 UCLASS()
 class WARRIOR_API AAARPGHeroCharacter : public AAARPGBaseCharacter
 {
 	GENERATED_BODY()
 
+public:
+	AAARPGHeroCharacter();
+
 protected:
 	virtual void BeginPlay() override;
+
+private:
+#pragma region Components
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* FollowCamera;
+
+#pragma endregion
 };
