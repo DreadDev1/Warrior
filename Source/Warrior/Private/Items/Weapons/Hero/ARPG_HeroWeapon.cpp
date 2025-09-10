a689c0d33f489c3fea00dbad05b731c894386955
DreadDev1 <kameen.avery@gmail.com>
2025-09-09 20:57:54 -0400

    Section 1 Lesson Setup Hero Weapon Class

---
+++ Source/Warrior/Private/Items/Weapons/Hero/ARPG_HeroWeapon.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Items/Weapons/Hero/ARPG_HeroWeapon.h"
+
+
+// Sets default values
+AARPG_HeroWeapon::AARPG_HeroWeapon()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AARPG_HeroWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AARPG_HeroWeapon::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/Warrior/Public/Items/Weapons/Hero/ARPG_HeroWeapon.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/Weapons/ARPG_WeaponBase.h"
+#include "ARPG_HeroWeapon.generated.h"
+
+UCLASS()
+class WARRIOR_API AARPG_HeroWeapon : public AARPG_WeaponBase
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AARPG_HeroWeapon();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
